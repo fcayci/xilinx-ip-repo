78cd5b74ad7587b2f85854a2fc5d949bcbd80da2
Furkan Cayci <furkanca02@gmail.com>
2018-12-06 11:44:02 +0300

    tab fix

--- objectbuffer_1.0/drivers/objectbuffer_v1_0/src/objectbuffer.c
+++ objectbuffer_1.0/drivers/objectbuffer_v1_0/src/objectbuffer.c
@@ -20,23 +20,23 @@
 int main()
 {
 	int status;
-    XGpio btn;
+	XGpio btn;
 
-    init_platform();
+	init_platform();
 
-    print("Initializing Buttons & Switches\n");
+	print("Initializing Buttons & Switches\n");
 	status = XGpio_Initialize(&btn, XPAR_AXI_GPIO_0_DEVICE_ID);
 	if (status != XST_SUCCESS) { return XST_FAILURE; }
 	// set the direction to input
 	XGpio_SetDataDirection(&btn, 1, 0xFF); // Channel 1
 
 	// Create a pointer to the base of the starting address of the buffer
-    volatile uint32_t *objs = (volatile uint32_t*)(XPAR_OBJECTBUFFER_0_S00_AXI_BASEADDR);
+	volatile uint32_t *objs = (volatile uint32_t*)(XPAR_OBJECTBUFFER_0_S00_AXI_BASEADDR);
 
-    print("Starting...\n\r");
+	print("Starting...\n\r");
 
-    uint8_t xdir = 0;
-    uint16_t obj1x, obj1y = 0;
+	uint8_t xdir = 0;
+	uint16_t obj1x, obj1y = 0;
 	uint16_t obj2x, obj2y = 0;
 	uint32_t background = 0xFFFFF00; // Start with yellow
 
@@ -53,11 +53,11 @@ int main()
 	objs[1] = (obj1x << 16 | obj1y);
 	objs[2] = (obj2x << 16 | obj2y);
 
-    print("Hello World\n\r");
-    while(1) {
+	print("Hello World\n\r");
+	while(1) {
 
-    	// Read buttons and update box location
-    	u32 b = XGpio_DiscreteRead(&btn, 1);
+		// Read buttons and update box location
+		u32 b = XGpio_DiscreteRead(&btn, 1);
 		if (1 == b) {
 			if (720-50 > obj1y) obj1y +=1;
 		}
@@ -69,10 +69,10 @@ int main()
 		if (8 == b) { background += 16; }
 
 		// calculate new ball location
-    	obj2x = (0 == xdir) ? obj2x+1 : obj2x-1;
+		obj2x = (0 == xdir) ? obj2x+1 : obj2x-1;
 
-    	if (992 < obj2x) xdir = 1;
-    	else if (65 > obj2x) xdir = 0;
+		if (992 < obj2x) xdir = 1;
+		else if (65 > obj2x) xdir = 0;
 
 		// update objects
 		objs[0] = background;
@@ -80,9 +80,9 @@ int main()
 		objs[2] = (obj2x << 16 | obj2y);
 
 		// wait aimlessly..
-    	for(int i=0; i<200000;i++);
-    }
+		for(int i=0; i<200000;i++);
+	}
 
-    cleanup_platform();
-    return 0;
+	cleanup_platform();
+	return 0;
 }
